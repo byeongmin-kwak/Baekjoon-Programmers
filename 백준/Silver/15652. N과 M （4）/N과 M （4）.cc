b3b91bf6f92260c5653725965094f005df2b83f5
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2022-07-16 18:08:09 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#define MAX 8
+using namespace std;
+
+int n, m;
+int arr[MAX];
+
+void dfs(int num, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	else {
+		for (int i = num; i <= n; i++) {
+			arr[cnt] = i;
+			dfs(i, cnt + 1);
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	dfs(1, 0);
+}
