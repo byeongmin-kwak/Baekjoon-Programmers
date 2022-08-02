366324e697155449d78a6d0bd8a380ba96492c9d
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2022-08-02 16:19:18 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -5,30 +5,19 @@ int N;
 long long arr[1000001];
 
 long long dp(int x) {
-	if (x == 0) {
-		arr[x] = 0;
-		return arr[x];
+	for (int i = 3; i <= N; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 15746;
 	}
-	else if (x == 1) {
-		arr[x] = 1;
-		return arr[x];
-	}
-	else if (x == 2) {
-		arr[x] = 2;
-		return arr[x];
-	}
-	if (arr[x] != 0) {
-		return arr[x];
-	}
-	else {
-		arr[x] = (dp(x - 1) + dp(x - 2)) % 15746;
-		return arr[x];
-	}
+	return arr[x];
 }
 
 
 int main() {
 	cin >> N;
+
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
 
 	cout << dp(N);
 }
