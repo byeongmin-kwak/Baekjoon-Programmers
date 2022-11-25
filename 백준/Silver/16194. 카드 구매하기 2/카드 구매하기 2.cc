ce903d869d4223d7ddb7c540f8a3ae9aa9de6fb4
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2022-11-25 17:46:15 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int dp[1001];
+int arr[1001];
+
+int main() {
+    
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+    
+    dp[1] = arr[1];
+    
+    for (int i = 2; i <= n; i++) {
+        dp[i] = arr[i];
+        for (int j = 1; j <= i / 2; j++) {
+            dp[i] = min(dp[i], dp[j]+dp[i-j]);
+        }
+    }
+    
+    cout << dp[n] << endl;
+    
+}
