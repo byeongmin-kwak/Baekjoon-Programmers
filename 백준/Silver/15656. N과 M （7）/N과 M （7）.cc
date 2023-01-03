987cadbe1bb70947913049c71fb121d2ad9d8cbd
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-01-03 14:20:58 +0900

    [Silver III] Title: N과 M (7), Time: 684 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int* arr = new int[N];
+int* temp = new int[N];
+//bool* visited = new bool[N];
+
+void dfs(int depth) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            cout << temp[i] << ' ';
+        }
+        cout << '\n';
+        
+        return;
+    }
+    for (int i = 0; i < N; i++) {
+//        if (visited[i]) {
+//            continue;
+//        }
+//        visited[i] = true;
+        temp[depth] = arr[i];
+        dfs(depth+1);
+//        visited[i] = false;
+    }
+}
+
+
+int main() {
+    
+    cin >> N >> M;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    
+    sort(arr, arr+N);
+    
+    dfs(0);
+    
+    
+}
+
