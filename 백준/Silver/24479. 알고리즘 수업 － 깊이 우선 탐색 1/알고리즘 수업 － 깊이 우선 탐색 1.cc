b6d92e4d90d891a1367bf802368e1b15bfd5d282
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-02-01 18:15:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 92 ms, Memory: 10928 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+ 
+vector<int> arr[100001];
+vector<int> visited(100001, 0);
+int cnt = 0;
+
+void dfs(int start) {
+    visited[start] = ++cnt;
+    
+    for (auto& edge : arr[start]) {
+        if (visited[edge])
+            continue;
+        dfs(edge);
+    }
+}   
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int N, M, R;
+    
+    cin >> N >> M >> R;
+    
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        arr[u].push_back(v);
+        arr[v].push_back(u);
+    }
+    for (int i = 1; i <= N; i++) {
+        sort(arr[i].begin(), arr[i].end());
+    }
+    
+    dfs(R);
+    
+    for (int i = 1; i <= N; i++) {
+        cout << visited[i] << '\n';
+    }
+}
