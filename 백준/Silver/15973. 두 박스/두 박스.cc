fa3fa452f5b29cbd6810942404bb9423dbaf4e9b
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-02-27 18:32:25 +0900

    [Silver I] Title: 두 박스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15973. 두 박스/두 박스.cc
+++ 백준/Silver/15973. 두 박스/두 박스.cc
@@ -2,41 +2,30 @@
 
 using namespace std;
 
+struct line {
+  int s, e;
+};
+
+int inter(line a, line b) {
+  if (a.s > b.s) {
+    line tmp = a;
+    a = b;
+    b = tmp;
+  }
+  if (a.e < b.s) return 0;
+  else if (a.e == b.s) return 1;
+  else return 2;
+}
+
 int main() {
-    int x1, y1, x2, y2, _x1, _y1, _x2, _y2;
-    
-    cin >> x1 >> y1 >> x2 >> y2;
-    cin >> _x1 >> _y1 >> _x2 >> _y2;
-    
-    if(x1 > _x1){
-            swap(x1, _x1);
-            swap(y1, _y1);
-            swap(x2, _x2);
-            swap(y2, _y2);
-        }
-    
-    if(x2 == _x1){
-        if(y2 == _y1 || y1 == _y2){
-            cout << "POINT" << "\n";
-        }
-        else {
-            cout <<"LINE" << "\n";
-        }
-    }
-    else if(x2 > _x1){
-        if(y2 < _y1 || y1 > _y2) {
-            cout << "NULL" << "\n";
-        }
-        else if(y2 == _y1 || y1 == _y2) {
-            cout << "LINE" << "\n";
-        }
-        else {
-            cout << "FACE" << "\n";
-        }
-    }
-    else if(x2 < _x1) {
-        cout << "NULL" << "\n";
-    }
+  line x1, y1;
+  line x2, y2;
 
-
+  scanf("%d%d%d%d", &x1.s, &y1.s, &x1.e, &y1.e);
+  scanf("%d%d%d%d", &x2.s, &y2.s, &x2.e, &y2.e);
+  int ix = inter(x1, x2), iy = inter(y1, y2);
+  if (ix == 2 && iy == 2) printf("FACE");
+  else if (ix == 1 && iy == 2 || ix == 2 && iy == 1) printf("LINE");
+  else if (ix == 1 && iy == 1) printf("POINT");
+  else printf("NULL");
 }
