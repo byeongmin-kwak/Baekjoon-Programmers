898edbebca0555f4724e30c00c9ff8416b6e1352
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-03-19 13:38:26 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int arr[100][100];
+
+int main() {
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    for (int k = 0; k < N; k++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][k] && arr[k][j]) {
+                    arr[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+}
