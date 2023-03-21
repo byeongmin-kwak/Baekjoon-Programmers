18d23fb5f5b8481fa06759e8918ea95a78262857
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-03-21 22:41:42 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.cc
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.cc
@@ -1,51 +1,70 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
+
 using namespace std;
 
+int arr[200][200];
 int N;
-int space[64][64];
-bool check[64][64];
+bool check[200][200];
+queue<pair<int, int>> que;
 
-void game() {
-	queue<pair<int, int>> q;
+void dfs(int x, int y) {
+    if (x == N-1 && y == N-1) {
+        cout << "HaruHaru" << endl;
+        exit(0);
+    }
+    else if (x >= N || y >= N) {
+        return;
+    }
+    else {
+        if (arr[y][x] != 0) {
+            dfs(x+arr[y][x],y);
+            dfs(x,y+arr[y][x]);
+        }
+    }
+}
 
-	q.push({ 0,0 });
-	check[0][0] = true;
+void bfs() {
+    if (arr[0][0] != 0 && arr[0][0] < N) {
+        que.push({0,arr[0][0]});
+        que.push({arr[0][0],0});
+        
+    }
+    
+    while(!que.empty()) {
+        int y = que.front().first;
+        int x = que.front().second;
+        que.pop();
 
-	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
-		q.pop();
-
-		if (space[x][y] == -1) {
-			cout << "HaruHaru";
-			exit(0);
-		}
-		else if (space[x][y] == 0) continue;
-
-		if (y + space[x][y] < N && !check[x][y + space[x][y]]) {
-			q.push({ x, y + space[x][y] });
-			check[x][y + space[x][y]] = true;
-		}
-		if (x + space[x][y] < N && !check[x + space[x][y]][y]) {
-			q.push({ x + space[x][y], y });
-			check[x + space[x][y]][y] = true;
-		}
-	}
+        if (x == N-1 && y == N-1) {
+            cout << "HaruHaru" << endl;
+            exit(0);
+            
+        }
+        if (arr[y][x] != 0) {
+            if (x+arr[y][x] < N && check[y][x+arr[y][x]] == false) {
+                que.push({y,x+arr[y][x]});
+                check[y][x+arr[y][x]] = true;
+            }
+            if (y+arr[y][x] < N && check[y+arr[y][x]][x] == false) {
+                que.push({y+arr[y][x], x});
+                check[y+arr[y][x]][x] = true;
+            }
+        }
+    }
 }
 
 int main() {
-	cin.tie(0);
-	ios_base::sync_with_stdio(0);
-
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			cin >> space[i][j];
-	
-	game();
-
-	cout << "Hing";
-
-	return 0;
+    
+    cin >> N;
+    
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    
+    bfs();
+    
+    cout << "Hing" << endl;
+    
 }
