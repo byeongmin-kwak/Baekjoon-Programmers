c3e0f914505157c362a2fd225526116a8914ea3e
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-03-22 15:12:47 +0900

    [Silver IV] Title: 상근이의 여행, Time: 84 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int T, N, M;
+    
+    cin >> T;
+    
+    while(T--) {
+        cin >> N >> M;
+        
+        for (int i = 0; i < M; i++) {
+            int a, b;
+            cin >> a >> b;
+            
+        }
+        
+        cout << N-1 << '\n';
+    }
+    
+    
+}
