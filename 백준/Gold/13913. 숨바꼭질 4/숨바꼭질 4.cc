12c74a32ebf7457a61cca5877df2e320322c5ada
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-03-29 15:10:52 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 16 ms, Memory: 6424 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,114 +1,114 @@
-    #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-    using namespace std;
+using namespace std;
 
-    int N, K;
-    bool visited[1000000];
-    pair<int,string> p[1000000];
+int N, K;
+bool visited[100001];
+pair<int,string> p[100001];
 
-    void bfs(int N, int K) {
-        if (N == K) {
-            return;
-        }
-        visited[N] = true;
+void bfs(int N, int K) {
+    if (N == K) {
+        return;
+    }
+    visited[N] = true;
 
-        queue<int> que;
-        int a = N - 1;
-        int b = N + 1;
-        int c = N * 2;
+    queue<int> que;
+    int a = N - 1;
+    int b = N + 1;
+    int c = N * 2;
 
-        if (N > K) {
+    if (N > K) {
+        que.push(a);
+        p[a] = {1,"-1"};
+        visited[a] = true;
+    }
+    else {
+        if (a >= 0) {
             que.push(a);
             p[a] = {1,"-1"};
             visited[a] = true;
         }
-        else {
-            if (a >= 0) {
+        
+        if (b <= 100000) {
+            que.push(b);
+            p[b] = {1,"+1"};
+            visited[b] = true;
+        }
+        
+        if (c <= 100000) {
+            que.push(c);
+            p[c] = {1,"*2"};
+            visited[c] = true;
+        }
+    }
+
+    while(!que.empty()) {
+        int tmp = que.front();
+        que.pop();
+        if (tmp == K) {
+            break;
+        }
+        int a = tmp - 1;
+        int b = tmp + 1;
+        int c = tmp * 2;
+        
+        if (tmp > K) {
+            if (!visited[a]) {
                 que.push(a);
-                p[a] = {1,"-1"};
+                p[a] = {p[tmp].first+1,"-1"};
+                visited[a] = true;
+            }
+        }
+        else {
+            if (a >= 0 && !visited[a]) {
+                que.push(a);
+                p[a] = {p[tmp].first+1,"-1"};
                 visited[a] = true;
             }
             
-            if (b <= 100000) {
+            if (b <= 100000 && !visited[b]) {
                 que.push(b);
-                p[b] = {1,"+1"};
+                p[b] = {p[tmp].first+1,"+1"};
                 visited[b] = true;
             }
             
-            if (c <= 100000) {
+            if (c <= 100000 && !visited[c]) {
                 que.push(c);
-                p[c] = {1,"*2"};
+                p[c] = {p[tmp].first+1,"*2"};
                 visited[c] = true;
             }
         }
-
-        while(!que.empty()) {
-            int tmp = que.front();
-            que.pop();
-            if (tmp == K) {
-                break;
-            }
-            int a = tmp - 1;
-            int b = tmp + 1;
-            int c = tmp * 2;
-            
-            if (tmp > K) {
-                if (!visited[a]) {
-                    que.push(a);
-                    p[a] = {p[tmp].first+1,"-1"};
-                    visited[a] = true;
-                }
-            }
-            else {
-                if (a >= 0 && !visited[a]) {
-                    que.push(a);
-                    p[a] = {p[tmp].first+1,"-1"};
-                    visited[a] = true;
-                }
-                
-                if (!visited[b] && b <= 100000) {
-                    que.push(b);
-                    p[b] = {p[tmp].first+1,"+1"};
-                    visited[b] = true;
-                }
-                
-                if (!visited[c] && c <= 100000) {
-                    que.push(c);
-                    p[c] = {p[tmp].first+1,"*2"};
-                    visited[c] = true;
-                }
-            }
-        }
     }
+}
 
-    int main() {
-        cin >> N >> K;
-        
-        bfs(N, K);
-        
-        stack<int> st;
-        int ans = K;
+int main() {
+    cin >> N >> K;
+    
+    bfs(N, K);
+    
+    stack<int> st;
+    int ans = K;
+    st.push(ans);
+    
+    while(ans != N) {
+        if (p[ans].second == "-1") {
+            ans += 1;
+        }
+        else if (p[ans].second == "+1") {
+            ans -= 1;
+        }
+        else {
+            ans /= 2;
+        }
         st.push(ans);
         
-        while(ans != N) {
-            if (p[ans].second == "-1") {
-                ans += 1;
-            }
-            else if (p[ans].second == "+1") {
-                ans -= 1;
-            }
-            else {
-                ans /= 2;
-            }
-            st.push(ans);
-            
-        }
-        cout << p[K].first << '\n';
-        
-        while(!st.empty()) {
-            cout << st.top() << ' ';
-            st.pop();
-        }
-        
-        
     }
+    cout << p[K].first << '\n';
+    
+    while(!st.empty()) {
+        cout << st.top() << ' ';
+        st.pop();
+    }
+    
+    
+}
