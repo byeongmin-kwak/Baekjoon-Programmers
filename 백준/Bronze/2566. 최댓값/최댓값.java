6050a4121b8c8a3a96a657d3b1b0c5c10946aa82
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-05-13 09:27:43 +0900

    [Bronze III] Title: 최댓값, Time: 116 ms, Memory: 12940 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -2,28 +2,28 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) 
-	{
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
 
-		int nums[][] = new int[10][10];
-		int max = 0;
-		int x = 0, y = 0;
+        int nums[][] = new int[9][9];
+        int max = 0;
+        int x = 0, y = 0;
 
-		for (int i = 0; i < 9; i++) 
-		{
-			for (int j = 0; j < 9; j++) 
-			{
-				nums[i][j] = sc.nextInt();
-				if (max < nums[i][j]) 
-				{
-					max = nums[i][j];
-					x = i; y = j;
-				}
-			}
-		}
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                nums[i][j] = sc.nextInt();
+                if (max < nums[i][j])
+                {
+                    max = nums[i][j];
+                    x = i; y = j;
+                }
+            }
+        }
 
-		System.out.println(max);
-		System.out.println((x + 1) + " " + (y + 1));
-	}
+        System.out.println(max);
+        System.out.println((x + 1) + " " + (y + 1));
+    }
 }
