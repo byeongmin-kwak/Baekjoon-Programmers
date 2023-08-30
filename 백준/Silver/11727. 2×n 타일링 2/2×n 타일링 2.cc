e75c6582d408bd8f069342393946474cc9cf8a3c
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2023-08-30 16:57:38 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,21 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-long long dp[1001];
+int arr[1001];
 
 int main() {
-    
-    int n;
-    cin >> n;
-
-    dp[1] = 1;
-    dp[2] = 3;
-    
-    for (int i = 3; i <= n; i++) {
-        dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
-    }
-    
-    cout << dp[n];
-    
+  
+  int n;
+  cin >> n;
+  
+  arr[1] = 1;
+  arr[2] = 3;
+  
+  for (int i = 3; i <= n; i++) {
+    arr[i] = (arr[i-2]*2 + arr[i-1]) % 10007;
+  }
+  
+  cout << arr[n];
+  
 }
