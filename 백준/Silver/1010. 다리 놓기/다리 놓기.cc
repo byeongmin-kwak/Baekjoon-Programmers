69f5ca5e170af96eac23045dbd68ccd6e5b0c270
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2024-11-23 09:47:52 +0900

    [Silver V] Title: 다리 놓기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,36 +1,23 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-
 int main() {
-
-	int T, N, M;
-
-	cin >> T;
-
-	vector<vector<long long >> dp(31, vector<long long>(31, 0));
-	dp[0][0] = 1;
-	dp[1][0] = 1;
-	dp[1][1] = 1;
-
-	for (int i = 2; i <= 30; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (j == 0) {
-				dp[i][j] = 1;
-			}
-			else if (j == i) {
-				dp[i][j] = 1;
-			}
-			else {
-				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-			}
-		}
-	}
-
-	while (T--) {
-		cin >> N >> M;
-
-		cout << dp[M][N] << '\n';
-	}
+  
+  int T, N, M;
+  
+  cin >> T;
+  
+  while(T--) {
+    cin >> N >> M;
+    long long result = 1;
+    int tmp = 1;
+    for (int i = M; i > M-N; i--) {
+      result *= i;
+      result /= tmp;
+      tmp++;
+    }
+    cout << result << endl;
+  }
+  
 }
