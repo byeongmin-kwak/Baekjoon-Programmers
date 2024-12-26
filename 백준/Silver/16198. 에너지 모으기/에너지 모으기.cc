bcc031485cdf9b41362da3aada3451e987a1db2f
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2024-12-26 21:17:25 +0900

    [Silver I] Title: 에너지 모으기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, ans;
+vector<int> originalV;
+
+void input() {
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    int W;
+    cin >> W;
+    originalV.push_back(W);
+  }
+}
+
+void solve(vector<int> v, int sum) {
+  if (v.size() == 2) {
+    ans = max(sum, ans);
+    return;
+    
+  }
+  for (int i = 1; i < v.size()-1; i++) {
+    vector<int> copy = v;
+    copy.erase(copy.begin() + i);
+    solve(copy, sum + v[i-1] * v[i+1]);
+    
+  }
+}
+
+int main() {
+  
+  input();
+  
+  solve(originalV, 0);
+  
+  cout << ans;
+  
+}
