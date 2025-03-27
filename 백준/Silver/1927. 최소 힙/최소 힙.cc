dd185cc6589b80ce5dbae1f29e136171bb7d64c2
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-03-27 13:13:59 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.cc
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -1,36 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main() {
+  
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  
+  priority_queue<int, vector<int>, greater<int>> pq;
+  
+  int N;
+  cin >> N;
+  
+  int x;
+  for (int i = 0; i < N; i++) {
+    cin >> x;
     
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    
-    int N;
-    cin >> N;
-    
-    priority_queue<int, vector<int>, greater<int>> pqu;
-    
-    int num;
-    for (int i = 0 ; i < N; i++) {
-        cin >> num;
-        if (num == 0) {
-            if (pqu.empty()) {
-                cout << 0 << '\n';
-            }
-            else {
-                cout << pqu.top() << '\n';
-                pqu.pop();
-            }
-        }
-        else {
-            pqu.push(num);
-        }
+    if (x == 0) {
+      if (pq.size() > 0) {
+        cout << pq.top() << '\n';;
+        pq.pop();
+      } else {
+        cout << 0 << '\n';;
+      }
+    } else {
+      pq.push(x);
     }
-    
-    
+  }
 }
-
