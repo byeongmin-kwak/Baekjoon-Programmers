1ec3b30702e25c18a4d422812dcf301cc9177152
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-07-29 16:41:32 +0900

    [level 0] Title: 문자열 출력하기, Time: 52.44 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,14 +1,3 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    console.log(str);
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+console.log(input);
