0c7e8b3890a653e000ad60065a5e04a7fe9fedb2
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-08-11 13:34:01 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.76 ms, Memory: 33.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,13 @@
+function solution(A,B){
+    let answer = 0;
+
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => a - b);
+    
+    for (let i = 0; i < A.length; i++) {
+        answer += A[i] * B[B.length-i-1];
+    }
+    
+    
+    return answer;
+}
