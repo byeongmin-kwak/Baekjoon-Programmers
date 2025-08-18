af5d9835cfc76ef4477a44bed16882dac95a1287
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-08-18 17:03:51 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    let answer = 0;
+    
+    let dp = [];
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (let i = 3; i <= n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
+    }
+    
+    answer = dp[n];
+    
+    return answer;
+}
