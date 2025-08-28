c511c0384b46a113236b74722b5a83face6ec595
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-08-28 22:40:05 +0900

    [level 2] Title: [1차] 캐시, Time: 76.74 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    
+    if cacheSize == 0: 
+        return len(cities) * 5
+    
+    cache = deque()
+    
+    for elem in cities:
+        city = elem.lower()
+        if city in cache:
+            answer += 1
+            cache.remove(city)
+            cache.append(city)
+        else:
+            answer += 5
+            if len(cache) >= cacheSize:
+                cache.popleft()
+            cache.append(city)
+        
+    return answer
