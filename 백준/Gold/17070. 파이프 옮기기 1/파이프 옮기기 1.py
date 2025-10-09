65b014e3d7e098baecc01ca088f302a9bbd5983f
Byeongmin Kwak <71933999+byeongmin-kwak@users.noreply.github.com>
2025-10-10 08:57:57 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,45 +1,28 @@
-def dfs(y1, x1, y2, x2, house):
-    global N
-    if y2 == N - 1 and x2 == N - 1:
-        global answer
-        answer += 1
-        return
-    
-    # 가로 
-    if y1 == y2:
-        # 가로 이동
-        if x2 + 1 < N and house[y2][x2+1] == 0:
-            dfs(y2, x2, y2, x2+1, house)
-        # 대각선 이동
-        if x2 + 1 < N and y2 + 1 < N and house[y2][x2+1] == 0 and house[y2+1][x2] == 0 and house[y2+1][x2+1] == 0:
-            dfs(y2, x2, y2+1, x2+1, house)
-    # 세로
-    elif x1 == x2:
-        # 세로 이동
-        if y2 + 1 < N and house[y2+1][x2] == 0:
-            dfs(y2, x2, y2+1, x2, house)
-        # 대각선 이동
-        if x2 + 1 < N and y2 + 1 < N and house[y2][x2+1] == 0 and house[y2+1][x2] == 0 and house[y2+1][x2+1] == 0:
-            dfs(y2, x2, y2+1, x2+1, house)
-    # 대각선
-    else:
-        # 가로 이동
-        if x2 + 1 < N and house[y2][x2+1] == 0:
-            dfs(y2, x2, y2, x2+1, house)
-        # 세로 이동
-        if y2 + 1 < N and house[y2+1][x2] == 0:
-            dfs(y2, x2, y2+1, x2, house)
-        # 대각선 이동
-        if x2 + 1 < N and y2 + 1 < N and house[y2][x2+1] == 0 and house[y2+1][x2] == 0 and house[y2+1][x2+1] == 0:
-            dfs(y2, x2, y2+1, x2+1, house)
-
-    return
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-house = [list(map(int, input().split())) for _ in range(N)]
+board = [list(map(int, input().split())) for _ in range(N)]
 
-answer = 0
+dp = [[[0] * 3 for _ in range(N)] for _ in range(N)]
 
-dfs(0, 0, 0, 1, house)
+# 초기 상태: (0,0)-(0,1) 가로로 시작
+dp[0][1][0] = 1
 
-print(answer)
+for y in range(N):
+    for x in range(2, N):  # (0,0)-(0,1) 이미 시작했으므로 x=2부터
+        if board[y][x] == 1:
+            continue  # 벽이면 불가능
+
+        # 가로
+        dp[y][x][0] = dp[y][x-1][0] + dp[y][x-1][2]
+
+        # 세로
+        if y > 0:
+            dp[y][x][1] = dp[y-1][x][1] + dp[y-1][x][2]
+
+        # 대각선
+        if y > 0 and board[y-1][x] == 0 and board[y][x-1] == 0:
+            dp[y][x][2] = dp[y-1][x-1][0] + dp[y-1][x-1][1] + dp[y-1][x-1][2]
+
+print(sum(dp[N-1][N-1]))
